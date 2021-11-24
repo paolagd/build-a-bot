92f9647f0d0fd83195af12fe34bb294bcb60cd2e
Paola Garcia <pacri14@gmail.com>
2021-11-23 22:58:39 -0500

    Adds  event example

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -8,15 +8,20 @@
           <span v-if="selectedRobot.head.onSale"> ON SALE!</span>
         </div>
       </div> -->
-      <PartSelector :parts="availableParts.heads" position="top"/>
+      <PartSelector :parts="availableParts.heads" position="top"
+      @partSelected="part => selectedRobot.head=part" />
     </div>
     <div class="middle-row">
-      <PartSelector :parts="availableParts.arms" position="left"/>
-      <PartSelector :parts="availableParts.torsos" position="center"/>
-      <PartSelector :parts="availableParts.arms" position="right"/>
+      <PartSelector :parts="availableParts.arms" position="left"
+      @partSelected="part => selectedRobot.leftArm=part"/>
+      <PartSelector :parts="availableParts.torsos" position="center"
+      @partSelected="part => selectedRobot.torso=part"/>
+      <PartSelector :parts="availableParts.arms" position="right"
+      @partSelected="part => selectedRobot.rightArm=part"/>
     </div>
     <div class="bottom-row">
-      <PartSelector :parts="availableParts.bases" position="bottom"/>
+      <PartSelector :parts="availableParts.bases" position="bottom"
+      @partSelected="part => selectedRobot.base=part"/>
     </div>
     <div>
       <h1>Cart</h1>
